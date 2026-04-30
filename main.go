871e345d24d759008b882223b21231ad7314a3e0
slashdeev <rocha.brunoxd@gmail.com>
2026-04-29 22:02:37 -0300

    adds & refactor: new feature symbols, crypto/rand added and recoded the whole thing.

--- main.go
+++ main.go
@@ -1,36 +1,61 @@
 package main
 
 import (
+	"crypto/rand"
 	"flag"
 	"fmt"
-	"math/rand"
+	"math/big"
+	"os"
 	"strings"
 )
 
-const charList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()-_=+[]{}<>?"
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const numbers = "0123456789"
+const symbols = "!@#$%^&*()-_=+"
 
-func generatePassword(length int) string {
-	var stringBuilder strings.Builder
-
+func generatePassword(length int, pool string) (string, error) {
+	var password strings.Builder
+	max := big.NewInt(int64(len(pool)))
+	
 	for i := 0; i < length; i++ {
-		randomIndex := rand.Intn(len(charList))
-		stringBuilder.WriteByte(charList[randomIndex])
+		number, err := rand.Int(rand.Reader, max)
+		if err != nil {
+			return "", err
+		}
+
+		password.WriteByte(pool[number.Int64()])
 	}
 
-	return stringBuilder.String()
+	return password.String(), nil
 }
 
 func main() {
-	lengthPtr := flag.Int("length", 12, "The length of characters.")
-	quantityPtr := flag.Int("quantity", 1, "The amount of passwords.")
+	lenghtPtr := flag.Int("len", 12, "The length of characters.")
+	quantityPtr := flag.Int("quantity", 5, "The amount of passwords.")
+	symbolsPtr := flag.Bool("symbols", false, "Include special characters.")
+
 	flag.Parse()
 
-	if *lengthPtr <= 0 || *quantityPtr <= 0 {
-		fmt.Println("[ERROR]: Length / Quantity must be a positive number!")
-		return
+	pool := uppercase + lowercase + numbers
+	if *symbolsPtr {
+		pool += symbols
+	}
+
+	if *lenghtPtr <= 0 || *quantityPtr <= 0 {
+		fmt.Fprint(os.Stderr, "Length / Quantity must be positive!")
+		os.Exit(1)
 	}
 
 	for i := 0; i < *quantityPtr; i++ {
-		fmt.Println(generatePassword(*lengthPtr))
+		password, err := generatePassword(*lenghtPtr, pool)
+		if err != nil {
+			fmt.Fprint(os.Stderr, "Something went wrong while trying to generate the password.")
+			fmt.Fprint(os.Stderr, err.Error())
+
+			os.Exit(1)
+		}
+
+		fmt.Println(password)
 	}
 }
